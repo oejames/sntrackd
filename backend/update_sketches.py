5fe6330dc738ddbfb8afe9f82a1b86d816ae4343
Ore James <jamesore200@gmail.com>
2026-05-19 18:40:24 -0500

    added last few sketches

--- backend/update_sketches.py
+++ backend/update_sketches.py
@@ -78,7 +78,6 @@ def check_for_new_sketches():
         youtube = get_youtube_client()
         videos = get_channel_videos(youtube)
 
-        videos = videos[30:]
         
         for video in videos:
             try:
